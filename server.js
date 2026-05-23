4c833edd2c4a97c1381ff1419974d3de0456438f
PowerPy Deploy <deploy@powerpy.com>
2026-05-23 15:35:32 +0000

    SEO: meta tags, OG, schema.org, sitemap, robots.txt

--- server.js
+++ server.js
@@ -204,7 +204,8 @@ const MIME = {
   '.json': 'application/json', '.png': 'image/png', '.jpg': 'image/jpeg',
   '.jpeg': 'image/jpeg', '.gif': 'image/gif', '.svg': 'image/svg+xml',
   '.ico': 'image/x-icon', '.woff': 'font/woff', '.woff2': 'font/woff2',
-  '.ttf': 'font/ttf', '.webp': 'image/webp'
+  '.ttf': 'font/ttf', '.webp': 'image/webp',
+  '.xml': 'application/xml', '.txt': 'text/plain'
 };
 
 function serveStatic(req, res) {
